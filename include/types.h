cb1787c154cc13eabe43a3f4c64304f775f06495
Kaan Olcay <71110056+KNCn23@users.noreply.github.com>
2026-05-21 12:27:48 +0300

    Add include/types.h

---
+++ include/types.h
@@ -0,0 +1,10 @@
+#ifndef TYPES_H
+#define TYPES_H
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+typedef unsigned long  u64;
+typedef signed   long  i64;
+typedef unsigned long  size_t;
+#define NULL ((void *)0)
+#endif
